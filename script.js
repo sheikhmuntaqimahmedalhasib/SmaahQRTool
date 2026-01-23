42257e4b83ec04a9d8de070b4bfa7a68a9d55b14
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-01-23 12:05:17 +0600

    smaahqrtool

--- script.js
+++ script.js
@@ -36,13 +36,33 @@ function generateQR(){
 
 function downloadQR(){
     const img = qrBox.querySelector("img");
-    if(!img) return;
-    const a = document.createElement("a");
-    a.href = img.src;
-    a.download = "SmaahTextToQrCode.png";
-    a.click();
-}
+    if(!img){
+        alert("Generate QR first");
+        return;
+    }
 
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+
+    ctx.drawImage(img, 0, 0);
+
+    canvas.toBlob(function(blob){
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "SmaahTextToQr.png";
+
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        URL.revokeObjectURL(url);
+    }, "image/png");
+}
 
 function previewQR(input){
     const file = input.files[0];
